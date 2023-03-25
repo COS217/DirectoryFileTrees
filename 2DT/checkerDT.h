acb27ab549431dd50f285067c9ba8806a0fc1899
cmoretti <cmoretti@cs.princeton.edu>
2023-03-24 23:47:21 -0400

    checkerDT interface should note printing to stderr

--- 2DT/checkerDT.h
+++ 2DT/checkerDT.h
@@ -11,13 +11,15 @@
 
 /*
    Returns TRUE if oNNode represents a directory entry
-   in a valid state, or FALSE otherwise.
+   in a valid state, or FALSE otherwise. Prints explanation
+   to stderr in the latter case.
 */
 boolean CheckerDT_Node_isValid(Node_T oNNode);
 
 /*
    Returns TRUE if the hierarchy is in a valid state or FALSE
-   otherwise.  The data structure's validity is based on a boolean
+   otherwise.  Prints explanation to stderr in the latter case.
+   The data structure's validity is based on a boolean
    bIsInitialized indicating whether the DT is in an initialized
    state, a Node_T oNRoot representing the root of the hierarchy, and
    a size_t ulCount representing the total number of directories in
